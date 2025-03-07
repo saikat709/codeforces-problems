340bcd49c13ffef359f1a8074f85101fcc4291df
Md. Saikat Islam <saikatislam709@gmail.com>
2025-03-07 21:18:19 +0600

    1584B

---
+++ 1584B_Coloring_Rectangles.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+void solve() {
+    int n, m; cin >> n >> m;
+    ll ans = ceil(n*m/3.0);
+    cout << ans << '\n';
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0), cout.tie(0);
+
+    // freopen( "_in.txt",  "r", stdin  );
+    // freopen( "_out.txt", "w", stdout );
+
+    int tt;
+    cin >> tt;
+    while (tt--) solve();
+    return 0;
+}
